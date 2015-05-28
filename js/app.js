391611f324506469a532b21459b1490bed6a5b9a
Helder <helder.correia@mrm-meteorite.com>
2015-05-28 18:04:09 +0100

    a note was added about the auto detect renderer method that was used to get the PIXI renderer instance

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 (function () {
 
 	// renderer instance
+	// the `autoDetectRenderer` checks if WebGL is available or fallback canvas
 	var renderer = PIXI.autoDetectRenderer(400, 300),
 
 	// create pixi stage
 	stage = new PIXI.Stage(0xFFFFFF);
 
 	// place it in to the DOM
-	document.body.appendChild(renderer.view);
+	document.querySelector('.ns-pixijs-test').appendChild(renderer.view);
 
 }());
