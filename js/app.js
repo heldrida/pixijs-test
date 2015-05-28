6d733911152dfb355c72cc195f65ad3c7c9c9f92
Helder <heldrida@gmail.com>
2015-05-28 18:17:55 +0100

    a skull image was added to the project

--- js/app.js
+++ js/app.js
@@ -5,9 +5,34 @@
 	var renderer = PIXI.autoDetectRenderer(400, 300),
 
 	// create pixi stage
-	stage = new PIXI.Stage(0xFFFFFF);
+	stage = new PIXI.Container();
 
 	// place it in to the DOM
 	document.querySelector('.ns-pixijs-test').appendChild(renderer.view);
+
+	requestAnimationFrame(animate);
+
+	function animate() {
+
+		requestAnimationFrame(animate);
+
+		skull.rotation += 0.1;
+
+		// render the `stage` instance
+		renderer.render(stage);
+
+	}
+
+	// create and add a sprite
+	var texture = PIXI.Texture.fromImage('img/skull.png?201505281812'),
+
+	// creates a texture from an image path
+	skull = new PIXI.Sprite(texture);
+
+	// center the sprite anchor point
+	skull.anchor.x = 0.5;
+	skull.anchor.y = 0.5;
+
+	stage.addChild(skull);
 
 }());
